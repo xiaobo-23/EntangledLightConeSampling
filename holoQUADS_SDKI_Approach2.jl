17d74534c301cf92e67fae7aa4ab4a973ea2b86f
Bo Xiao <bxiao@flatironinstitute.org>
2023-05-02 16:51:18 -0400

    add the left light cone structure

--- holoQUADS_SDKI_Approach2.jl
+++ holoQUADS_SDKI_Approach2.jl
@@ -27,7 +27,7 @@ function sample(m :: MPS, j :: Int, observable_type :: AbstractString)
         error("sample: MPS is not normalized, norm=$(norm(m[j]))")
     end
 
-    # Implement procedures to sample in Sx, Sy and Sz basis
+    # Define projectors in the Sz basis
     Sx_projn = 1/sqrt(2) * [[1, 1], [1, -1]]
     Sy_projn = 1/sqrt(2) * [[1, 1.0im], [1, -1.0im]]
     Sz_projn = [[1, 0], [0, 1]]
@@ -78,3 +78,29 @@ function sample(m :: MPS, j :: Int, observable_type :: AbstractString)
     end
     return result
 end 
+
+# Contruct layers of two-site gates including the Ising interaction and longitudinal fileds in the left light cone.
+function left_light_cone(number_of_gates :: Int, parity :: Int, longitudinal_field :: Float64, Δτ :: Float64, tmp_sites)
+    gates = ITensor[]
+
+    for ind in 1 : number_of_gates
+        tmp_index = 2 * ind - parity
+        s1 = tmp_sites[tmp_index]
+        s2 = tmp_sites[tmp_index + 1]
+       
+        if tmp_index - 1 < 1E-8
+            coeff₁ = 2
+            coeff₂ = 1
+        else
+            coeff₁ = 1
+            coeff₂ = 1
+        end
+
+        # hj = coeff₁ * longitudinal_field * op("Sz", s1) * op("Id", s2) + coeff₂ * longitudinal_field * op("Id", s1) * op("Sz", s2)
+        # hj = π * op("Sz", s1) * op("Sz", s2) + coeff₁ * longitudinal_field * op("Sz", s1) * op("Id", s2) + coeff₂ * longitudinal_field * op("Id", s1) * op("Sz", s2)
+        hj = π/2 * op("Sz", s1) * op("Sz", s2) + coeff₁ * longitudinal_field * op("Sz", s1) * op("Id", s2) + coeff₂ * longitudinal_field * op("Id", s1) * op("Sz", s2)
+        Gj = exp(-1.0im * Δτ * hj)
+        push!(gates, Gj)
+    end
+    return gates
+end
