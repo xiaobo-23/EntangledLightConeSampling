25c20d88ee8f6972531378b323096fb5c21639ba
Bo Xiao <bxiao@flatironinstitute.org>
2023-06-16 09:45:26 -0400

    Timing each block in the TEBD code

--- src/TEBD_SDKI_Rectangle.jl
+++ src/TEBD_SDKI_Rectangle.jl
@@ -8,14 +8,15 @@ using Base: Float64, Real, Integer
 using Random
 using Dates
 using TimerOutputs
-using MKL
 # using AppleAccelerate
 # using AppleAccelerateLinAlgWrapper
 
+
+using MKL
 using LinearAlgebra
 BLAS.set_num_threads(8)
 
-const to = TimerOutput()
+const time_machine = TimerOutput()
 ITensors.disable_warn_order()
 
 include("Entanglement.jl")
@@ -26,27 +27,30 @@ let
     N = 100
     cutoff = 1E-8
     Δτ = 1.0 
-    ttotal = 10
+    ttotal = 15
     h = 0.2                                            # an integrability-breaking longitudinal field h 
 
     # Make an array of 'site' indices && quantum numbers are not conserved due to the transverse fields
-    s = siteinds("S=1/2", N; conserve_qns = false);     # s = siteinds("S=1/2", N; conserve_qns = true)
+    s = siteinds("S=1/2", N; conserve_qns = false)
 
     # Construct a layer (odd & even) of gates for the SDKI model
-    @timeit to "Set up problem" begin
-    gates = Vector{ITensor}()
-    even_layer = build_a_layer_of_gates!(2, N-2, N, h, Δτ, s, gates)
-    # for tmp₁ in even_layer
-    #     push!(gates, tmp₁)
-    # end
+    @timeit time_machine "Generating sequences of gates" begin
+        gates = Vector{ITensor}()
+        even_layer = build_a_layer_of_gates!(2, N-2, N, h, Δτ, s, gates)
+        odd_layer = build_a_layer_of_gates!(1, N-1, N, h, Δτ, s, gates)
+        kick_gates = build_kick_gates(s, 1, N)
+    end
 
-    odd_layer = build_a_layer_of_gates!(1, N-1, N, h, Δτ, s, gates)
-    # for tmp₂ in odd_layer
-    #     push!(gates, tmp₂)
-    # end
+    # # for tmp₁ in even_layer
+    # #     push!(gates, tmp₁)
+    # # end
 
-    # Construct a layer of kicked gates
-    kick_gates = build_kick_gates(s, 1, N)
+    # # for tmp₂ in odd_layer
+    # #     push!(gates, tmp₂)
+    # # end
+
+    # # Construct a layer of kicked gates
+    # kick_gates = build_kick_gates(s, 1, N)
 
     # Initialize the wavefunction using a Neel state
     states = [isodd(n) ? "Up" : "Dn" for n = 1 : N]
@@ -66,86 +70,94 @@ let
     timeSlices = Int(ttotal / Δτ) + 1
     println("Total number of time slices that need to be saved is : $(timeSlices)")
     
-    # Local observables including various one-point functions
-    Sx = Array{ComplexF64}(undef, timeSlices, N)
-    Sy = Array{ComplexF64}(undef, timeSlices, N)
-    Sz = Array{ComplexF64}(undef, timeSlices, N)
-    
-    # Equal-time observables including various two-point functions
-    Cxx = Array{ComplexF64}(undef, timeSlices, N * N)
-    Cyy = Array{ComplexF64}(undef, timeSlices, N * N)
-    Czz = Array{ComplexF64}(undef, timeSlices, N * N)
-    SvN = Array{Float64}(undef, timeSlices, N - 1)
-    
-    # Take measurements of the initial wavefunction
-    Sx[1, :] = expect(ψ_copy, "Sx"; sites = 1 : N)
-    # Sy[1, :] = expect(ψ_copy, "Sy"; sites = 1 : N)
-    Sz[1, :] = expect(ψ_copy, "Sz"; sites = 1 : N)
+    @timeit time_machine "Allocate memory" begin
+        # Local observables including various one-point functions
+        Sx = Array{ComplexF64}(undef, timeSlices, N)
+        Sy = Array{ComplexF64}(undef, timeSlices, N)
+        Sz = Array{ComplexF64}(undef, timeSlices, N)
+        
+        # Equal-time observables including various two-point functions
+        Cxx = Array{ComplexF64}(undef, timeSlices, N * N)
+        Cyy = Array{ComplexF64}(undef, timeSlices, N * N)
+        Czz = Array{ComplexF64}(undef, timeSlices, N * N)
 
-    Cxx[1, :] = correlation_matrix(ψ_copy, "Sx", "Sx"; sites = 1 : N)
-    # Cyy[1, :] = correlation_matrix(ψ_copy, "Sy", "Sy"; sites = 1 : N)
-    Czz[1, :] = correlation_matrix(ψ_copy, "Sz", "Sz"; sites = 1 : N)
+        # Entanglement etc. for the whole chain
+        SvN = Array{Float64}(undef, timeSlices, N - 1)
+        Bond = Array{Float64}(undef, timeSlices, N - 1)
+    end
     
-    SvN[1, :] = entanglement_entropy(ψ_copy, N)
-    # append!(ψ_overlap, abs(inner(ψ, ψ_copy)))
+    @timeit time_machine "Measure the initial wavefunction" begin
+        Sx[1, :] = expect(ψ_copy, "Sx"; sites = 1 : N)
+        # Sy[1, :] = expect(ψ_copy, "Sy"; sites = 1 : N)
+        Sz[1, :] = expect(ψ_copy, "Sz"; sites = 1 : N)
 
+        Cxx[1, :] = correlation_matrix(ψ_copy, "Sx", "Sx"; sites = 1 : N)
+        # Cyy[1, :] = correlation_matrix(ψ_copy, "Sy", "Sy"; sites = 1 : N)
+        Czz[1, :] = correlation_matrix(ψ_copy, "Sz", "Sz"; sites = 1 : N)
+        
+        SvN[1, :] = entanglement_entropy(ψ_copy, N)
+        Bond[1, :] = obtain_bond_dimension(ψ_copy, N)
+    end
+    
     distance = Int(1.0 / Δτ)
     index = 2
-    end
+
     for time in 0.0 : Δτ : ttotal
         time ≈ ttotal && break
         
         # Apply the kicked gates at integer time
-        @timeit to "kick gates" if (abs((time / Δτ) % distance) < 1E-8)
+        @timeit time_machine "Applying one-site gates" if (abs((time / Δτ) % distance) < 1E-8)
             ψ_copy = apply(kick_gates, ψ_copy; cutoff)
             normalize!(ψ_copy)
             # append!(ψ_overlap, abs(inner(ψ, ψ_copy)))
         end
 
         # Apply the two-site gates which include the Ising interaction and longitudinal fields
-        @timeit to "two-site gates" begin
+        @timeit time_machine "Applying two-site gates" begin
             ψ_copy = apply(gates, ψ_copy; cutoff)
             normalize!(ψ_copy)
         end
         
         # Local observables e.g. Sx, Sz
-        @timeit to "Compute expectation" begin 
+        @timeit time_machine "Compute one-point function" begin 
             Sx[index, :] = expect(ψ_copy, "Sx"; sites = 1 : N) 
             # Sy[index, :] = expect(ψ_copy, "Sy"; sites = 1 : N)
             Sz[index, :] = expect(ψ_copy, "Sz"; sites = 1 : N)
         end
 
-        @timeit to "compute correlation functions" begin
+        @timeit time_machine "Compute two-point function" begin
         # Correlation functions e.g. Cxx, Czz
             Cxx[index, :] = correlation_matrix(ψ_copy, "Sx", "Sx"; sites = 1 : N)
             # Cyy[index, :] = correlation_matrix(ψ_copy, "Sy", "Sy"; sites = 1 : N)
             Czz[index, :] = correlation_matrix(ψ_copy, "Sz", "Sz"; sites = 1 : N)
+        end
+
+        @timeit time_machine "Compute von Neumann entanglement entropy and bond dimension" begin
             SvN[index, :] = entanglement_entropy(ψ_copy, N)
+            Bond[index, :] = obtain_bond_dimension(ψ_copy, N)
         end
 
         index += 1
         # append!(ψ_overlap, abs(inner(ψ, ψ_copy)))
         
-        # @show to
-        show(to)
+        @show to
+        @show SvN[index, :]
+        
+        # Store data into a hdf5 file
+        h5open("../Scalable_Data/TEBD_N$(N)_h$(h)_tau$(Δτ)_T$(ttotal)_cuttoff$(cutoff).h5", "w") do file
+            write(file, "Sx", Sx)
+            write(file, "Sy", Sy)
+            write(file, "Sz", Sz)
+            write(file, "Cxx", Cxx)
+            write(file, "Cyy", Cyy)
+            write(file, "Czz", Czz)
+            # write(file, "Wavefunction Overlap", ψ_overlap)
+            write(file, "Entropy", SvN)
+            write(file, "Initial Sx", Sx[1, :])
+            write(file, "Initial Sy", Sy[1, :])
+            write(file, "Initial Sz", Sz[1, :])
+        end
     end
 
-    
-    # Store data into a hdf5 file
-    file = h5open("TEBD_N$(N)_h$(h)_tau$(Δτ)_T$(ttotal).h5", "w")
-    write(file, "Sx", Sx)
-    write(file, "Sy", Sy)
-    write(file, "Sz", Sz)
-    write(file, "Cxx", Cxx)
-    write(file, "Cyy", Cyy)
-    write(file, "Czz", Czz)
-    # write(file, "Wavefunction Overlap", ψ_overlap)
-    write(file, "Entropy", SvN)
-    # write(file, "Time Sequence", timing)
-    write(file, "Initial Sx", Sx[1, :])
-    write(file, "Initial Sy", Sy[1, :])
-    write(file, "Initial Sz", Sz[1, :])
-    close(file)
-    
     return
 end  
