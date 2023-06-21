a59b204eaeaf2a36f29b8a59a6eba5696c09251a
Bo Xiao <bxiao@flatironinstitute.org>
2023-06-20 20:10:51 -0400

    Updates on the files to use disBatch on the cluster

---
+++ holoQUADS_Submission/Generate_disBatch_Input_File.py
@@ -0,0 +1,30 @@
+##################################################################
+## Generate input files for disBatch
+## Infinite MPS and the extended Hubard model in 1D 
+##################################################################
+
+import numpy as np
+import sys
+import csv
+import os 
+import shutil
+import math
+from io import StringIO
+
+
+def generate_input_file(input_index, task_file):
+    '''Generate corresponding folders and input files based on chemical potential'''
+    folder_name = "Sample" + "{}".format(input_index) + "/"
+    task_file.write("cd " + folder_name \
+        + " &&  julia --threads=1 holoQUADS_SDKI.jl" + " &> holoQUADS_" \
+        + "{}".format(input_index) + ".log" + "\n")
+    
+def main():
+    sample_list = np.arange(1, 251, 1)
+    location = os.path.dirname(os.path.realpath(__file__))
+
+    submit_file = open("T30", "a")
+    for tmp in sample_list:
+        generate_input_file(tmp, submit_file)
+    submit_file.close()    
+main()
