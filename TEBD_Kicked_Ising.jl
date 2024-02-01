e3828177743340d03574833436885af54a889c13
Bo Xiao <bxiao@flatironinstitute.org>
2024-02-01 16:15:47 -0500

    Add time-dependent spin correlators

--- TEBD_Kicked_Ising.jl
+++ TEBD_Kicked_Ising.jl
@@ -28,9 +28,10 @@ include("src/SDKI/ObtainBond.jl")
 let 
     N=100
     cutoff=1E-8
-    Δτ=1.0 
+    Δτ=1
     ttotal=5
     h=0.2                                              # an integrability-breaking longitudinal field h 
+    time_dependent=true
     # bond_dimension_upper_bound=10
 
     # Make an array of 'site' indices && quantum numbers are not conserved due to the transverse fields
@@ -45,12 +46,12 @@ let
     end
 
     # Initialize the wavefunction using a Neel state
-    states = [isodd(n) ? "Up" : "Dn" for n = 1 : N]
-    ψ = MPS(s, states)
+    # states = [isodd(n) ? "Up" : "Dn" for n = 1 : N]
+    # ψ = MPS(s, states)
+    ψ = randomMPS(s, linkdims = 16)
     ψ_copy = deepcopy(ψ)
     ψ_overlap = Complex{Float64}[]
 
-   
     timeSlices = Int(ttotal / Δτ) + 1
     println("Total number of time slices that need to be saved is : $(timeSlices)")
     
@@ -67,9 +68,11 @@ let
         Czz = Array{ComplexF64}(undef, timeSlices, N * N)
 
         # Time-dependent spin correlation funciton
-        Cxx_time = Array{ComplexF64}(undef, timeSlices, N)
-        # Cyy_time = Array{ComplexF64}(undef, timeSlices, N)
-        # Czz_time = Array{ComplexF64}(undef, timeSlices, N)
+        if time_dependent
+            Cxx_time = Array{ComplexF64}(undef, timeSlices, N)
+            # Cyy_time = Array{ComplexF64}(undef, timeSlices, N)
+            # Czz_time = Array{ComplexF64}(undef, timeSlices, N)
+        end
 
         # Entanglement etc. for the whole chain
         SvN = Array{Float64}(undef, timeSlices, N - 1)
@@ -93,31 +96,49 @@ let
     distance = Int(1.0 / Δτ)
     index = 2
 
-    # Choose a reference site and act on the initial wavefunction with the corresponding operator before doing any time evolution
-    reference = 1
-    reference_operator = op("Sz", s[reference])
-    ψ_copy = apply(reference_operator, ψ_copy; cutoff)
-    # ψ_copy = reference_operator * ψ_copy
-    normalize!(ψ_copy)
+    # Choose a reference site and act on the initial wavefunction with 
+    # the corresponding operator before doing any time evolution
+    if time_dependent
+        reference_position = 50
+        reference_operator = op("Sz", s[reference_position])
+        ψ_right = apply(reference_operator, ψ_copy; cutoff)
+        normalize!(ψ_right)
+        println("After applying the local spin operator, normalize the wavefunction leads to :")
+        @show inner(ψ_right', ψ_right)
+    end
     
 
-    # Real time dynamics of the SDKI model
+    # Time evolve the wavefunction using TEBD
     for time in 0.0 : Δτ : ttotal
         time ≈ ttotal && break
 
-
-        # Apply the kicked gates at integer time
+        # Apply the one-site gates which include the kicked transverse field at integer time
         @timeit time_machine "Applying one-site gates" if (abs((time / Δτ) % distance) < 1E-8)
-            # ψ_copy = apply(kick_gates, ψ_copy; cutoff, maxdim=bond_dimension_upper_bound)
+            @show time/Δτ, abs((time / Δτ) % distance), distance
             ψ_copy = apply(kick_gates, ψ_copy; cutoff)
             normalize!(ψ_copy)
+
+            if time_dependent
+                ψ_right = apply(kick_gates, ψ_right; cutoff)
+                normalize!(ψ_right)
+            end
+
+            # Setting a maximum bond dimension during the time evolution
+            # ψ_copy = apply(kick_gates, ψ_copy; cutoff, maxdim=bond_dimension_upper_bound)
         end
 
         # Apply the two-site gates which include the Ising interaction and longitudinal fields
         @timeit time_machine "Applying two-site gates" begin
-            # ψ_copy = apply(gates, ψ_copy; cutoff, maxdim=bond_dimension_upper_bound)
             ψ_copy = apply(gates, ψ_copy; cutoff)
             normalize!(ψ_copy)
+
+            if time_dependent
+                ψ_right = apply(gates, ψ_right; cutoff)
+                normalize!(ψ_right)
+            end
+
+            # Setting a maximum bond dimension during the time evolution
+            # ψ_copy = apply(gates, ψ_copy; cutoff, maxdim=bond_dimension_upper_bound)
         end
         
         # Local observables e.g. Sx, Sz
@@ -139,21 +160,25 @@ let
             Bond[index, :] = obtain_bond_dimension(ψ_copy, N)
         end
 
-        for site_ind in eachindex(collect(1:N))
-            tmp_ψ = deepcopy(ψ_copy)
-            time_operator = op("Sz", s[site_ind])
-            tmp_ψ = apply(time_operator, tmp_ψ; cutoff)
-            Cxx_time[index, site_ind] = inner(tmp_ψ, tmp_ψ)
+        
+        if time_dependent
+            for site_ind in eachindex(collect(1:N))
+                # @show site_ind
+                tmp_ψ = deepcopy(ψ_right)
+                time_operator = op("Sz", s[site_ind])
+                tmp_ψ = apply(time_operator, tmp_ψ; cutoff)
+                Cxx_time[index, site_ind] = inner(ψ_copy, tmp_ψ)
+            end
         end
 
+        # @show Bond[index, :]
         # @show SvN[index, :]
         # @show Sz[index, :]
-        @show Bond[index, :]
         
         index += 1
         
         # Store output data in a HDF5 file
-        h5open("data/kicked_ising/TEBD_N$(N)_h$(h)_tau$(Δτ)_T$(ttotal).h5", "w") do file
+        h5open("data/kicked_ising/TEBD_N$(N)_h$(h)_tau$(Δτ)_T$(ttotal)_random_ref$(reference_position).h5", "w") do file
             write(file, "Sx", Sx)
             write(file, "Sy", Sy)
             write(file, "Sz", Sz)
@@ -168,19 +193,8 @@ let
             write(file, "Sz0", Sz[1, :])
         end
     end
-    
-    # for site_ind in eachindex(collect(1:N))
-    #     tmp_ψ = deepcopy(ψ_copy)
-    #     time_operator = op("Sz", s[site_ind])
-    #     tmp_ψ = apply(time_operator, tmp_ψ; cutoff)
-    #     Cxx_time[site_ind] = inner(ψ, tmp_ψ)
-    # end
-
-    #  # Store output data in a HDF5 file
-    #  h5open("data/kicked_ising/TEBD_N$(N)_h$(h)_tau$(Δτ)_T$(ttotal).h5", "w") do file
-    #     write(file, "CxxTime", Cxx_time)
-    # end
-
+   
+    # Show information (time, memorty, etc.) about the time evolution
     @show time_machine
     return
 end  
