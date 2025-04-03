2396d6cacddab4a94929945f21cfec71fd7aa815
Bo Xiao <bxiao@flatironinstitute.org>
2025-04-02 21:32:06 -0400

    Implement the computation of two-point functions based on samples generated through deterministic sampling.

--- src/deterministic_sampling/deterministic_sample_truncated.jl
+++ src/deterministic_sampling/deterministic_sample_truncated.jl
@@ -91,9 +91,10 @@ function sample(m :: MPS, j :: Int, observable :: AbstractString)
 end
 
 
-# 08/16/2024
-# Sample a single-site MPS deterministically  
-function single_site_dsample(m :: MPS, j :: Int, observable :: AbstractString) 
+
+# 04/02/2025
+# Sample a MPS deterministically  
+function single_site_dsample(m :: MPS, j :: Int, observable :: AbstractString, state_string :: AbstractString) 
     # Set up the projection operator
     if observable == "Sx"
         tmp_projn = Sx_projn
@@ -122,14 +123,8 @@ function single_site_dsample(m :: MPS, j :: Int, observable :: AbstractString)
         An = A * dag(projn);
         pn = real(scalar(dag(An) * An))
 
-        # println("")
-        # println("")
-        # @show typeof(A), typeof(An), typeof(projn) 
-        # @show A, projn
-        # println("")
-        # println("")
-
-        sample_info = [string(index), pn, An]
+        output_string = state_string * string(index)
+        sample_info = [output_string, pn, An]
         push!(result_vector, sample_info)
     end
     # @show length(result_vector)
@@ -138,9 +133,56 @@ function single_site_dsample(m :: MPS, j :: Int, observable :: AbstractString)
 end
 
 
+# # 08/16/2024
+# # Sample a MPS deterministically   
+# function single_site_dsample(m :: MPS, j :: Int, observable :: AbstractString) 
+#     # Set up the projection operator
+#     if observable == "Sx"
+#         tmp_projn = Sx_projn
+#     elseif observable == "Sy"
+#         tmp_projn = Sy_projn
+#     elseif observable == "Sz"
+#         tmp_projn = Sz_projn
+#     else
+#         error("sample: the type of measurement doesn't exist")
+#     end
+
+#     # Sample the target observables
+#     result_vector = []
+#     A = m[j]
+#     tmpS = siteind(m, j)
+#     d = dim(tmpS)
+
+#     for index in 1 : d
+#         pn = 0.0
+#         An = ITensor()
+#         projn = ITensor(tmpS)
+#         # projn[tmpS => index] = 1.0
+#         projn[tmpS => 1] = tmp_projn[index][1]
+#         projn[tmpS => 2] = tmp_projn[index][2]
+
+#         An = A * dag(projn);
+#         pn = real(scalar(dag(An) * An))
+
+#         # println("")
+#         # println("")
+#         # @show typeof(A), typeof(An), typeof(projn) 
+#         # @show A, projn
+#         # println("")
+#         # println("")
+
+#         sample_info = [string(index), pn, An]
+#         push!(result_vector, sample_info)
+#     end
+#     # @show length(result_vector)
+#     # @show result_vector
+#     return result_vector
+# end
+
+
 let 
     # Initialize the random MPS
-    N = 100                  # Number of physical sites
+    N = 10                   # Number of physical sites
     h = 1.2                  # Transverse field
     # sites = siteinds("S=1/2", N; conserve_qns = false) 
     # state = [isodd(n) ? "Up" : "Dn" for n = 1 : N]
@@ -150,7 +192,7 @@ let
     println("Read in the wavefunction from the file and start the sampling process.")
     println("*************************************************************************************")
     println(" ")
-    file = h5open("data/Transverse_Ising_N100_h1.2_Wavefunction.h5", "r")
+    file = h5open("data/Transverse_Ising_N10_h1.2_Wavefunction.h5", "r")
     ψ = read(file, "Psi", MPS)
     Sz = expect(ψ, "Sz"; sites = 1 : N)
     Sx = expect(ψ, "Sx"; sites = 1 : N)
@@ -158,69 +200,69 @@ let
     @show Sx
     @show Czz
 
-    #***********************************************************************************
-    # Generate samples in the Sz basis based on the Born rule
-    #***********************************************************************************
-    Nₛ = 5000              # Number of samples
-    bitstring_Sz  = Array{Float64}(undef, Nₛ, N)
-    bitstring_Czz = Array{Float64}(undef, Nₛ, N * N)
-    for i = 1 : Nₛ
-        ψ_copy = deepcopy(ψ)
-        println("Generate bitstring #$(i)")
-        println("")
-        println("")
-        for j = 1 : 2 : N
-            tmp = sample(ψ_copy, j, "Sz")
-            normalize!(ψ_copy)
-            bitstring_Sz[i, j: j + 1] = tmp
-        end
-    end
-    bitstring_Sz[bitstring_Sz .== 1] .= 0.5
-    bitstring_Sz[bitstring_Sz .== 2] .= -0.5
-    # @show bitstring
+    # #***********************************************************************************
+    # # Generate samples in the Sz basis based on the Born rule
+    # #***********************************************************************************
+    # Nₛ = 5000              # Number of samples
+    # bitstring_Sz  = Array{Float64}(undef, Nₛ, N)
+    # bitstring_Czz = Array{Float64}(undef, Nₛ, N * N)
+    # for i = 1 : Nₛ
+    #     ψ_copy = deepcopy(ψ)
+    #     println("Generate bitstring #$(i)")
+    #     println("")
+    #     println("")
+    #     for j = 1 : 2 : N
+    #         tmp = sample(ψ_copy, j, "Sz")
+    #         normalize!(ψ_copy)
+    #         bitstring_Sz[i, j: j + 1] = tmp
+    #     end
+    # end
+    # bitstring_Sz[bitstring_Sz .== 1] .= 0.5
+    # bitstring_Sz[bitstring_Sz .== 2] .= -0.5
+    # # @show bitstring
     
-    # Compute the expectation value and standard deviation of one-point functions based on the samples
-    sample_ave_Sz = mean(bitstring_Sz, dims = 1)
-    sample_std_Sz = std(bitstring_Sz, corrected = true, dims = 1) / sqrt(Nₛ)
-    # @show sample_expectation 
-    # @show sample_std
+    # # Compute the expectation value and standard deviation of one-point functions based on the samples
+    # sample_ave_Sz = mean(bitstring_Sz, dims = 1)
+    # sample_std_Sz = std(bitstring_Sz, corrected = true, dims = 1) / sqrt(Nₛ)
+    # # @show sample_expectation 
+    # # @show sample_std
 
 
-    # Compute the expectation value and standard deviation of two-point functions based on the samples
-    for index in 1 : Nₛ
-        for i = 1 : N 
-            for j = 1 : N
-                bitstring_Czz[index, (i - 1) * N + j] = bitstring_Sz[index, i] * bitstring_Sz[index, j]
-            end
-        end
-    end
-    sample_ave_Czz = mean(bitstring_Czz, dims = 1)
-    sample_std_Czz = std(bitstring_Czz, corrected = true, dims = 1) / sqrt(Nₛ)
+    # # Compute the expectation value and standard deviation of two-point functions based on the samples
+    # for index in 1 : Nₛ
+    #     for i = 1 : N 
+    #         for j = 1 : N
+    #             bitstring_Czz[index, (i - 1) * N + j] = bitstring_Sz[index, i] * bitstring_Sz[index, j]
+    #         end
+    #     end
+    # end
+    # sample_ave_Czz = mean(bitstring_Czz, dims = 1)
+    # sample_std_Czz = std(bitstring_Czz, corrected = true, dims = 1) / sqrt(Nₛ)
     
 
-    #***********************************************************************************
-    # Generate samplesin the Sx basis based on the Born rule.
-    #***********************************************************************************
+    # #***********************************************************************************
+    # # Generate samplesin the Sx basis based on the Born rule.
+    # #***********************************************************************************
 
-    bitstring_Sx = Array{Float64}(undef, Nₛ, N)
-    for i = 1 : Nₛ
-        ψ_copy = deepcopy(ψ)
-        println("Generate bitstring #$(i)")
-        println("")
-        println("")
-        for j = 1 : 2 : N
-            tmp = sample(ψ_copy, j, "Sx")
-            normalize!(ψ_copy)
-            bitstring_Sx[i, j: j + 1] = tmp
-        end
-    end
-    bitstring_Sx[bitstring_Sx .== 1] .= 0.5
-    bitstring_Sx[bitstring_Sx .== 2] .= -0.5
-    # @show bitstring_Sx
+    # bitstring_Sx = Array{Float64}(undef, Nₛ, N)
+    # for i = 1 : Nₛ
+    #     ψ_copy = deepcopy(ψ)
+    #     println("Generate bitstring #$(i)")
+    #     println("")
+    #     println("")
+    #     for j = 1 : 2 : N
+    #         tmp = sample(ψ_copy, j, "Sx")
+    #         normalize!(ψ_copy)
+    #         bitstring_Sx[i, j: j + 1] = tmp
+    #     end
+    # end
+    # bitstring_Sx[bitstring_Sx .== 1] .= 0.5
+    # bitstring_Sx[bitstring_Sx .== 2] .= -0.5
+    # # @show bitstring_Sx
     
-    # Compute the expectation value and standard deviation of one-point functions based on the samples 
-    sample_ave_Sx = mean(bitstring_Sx, dims = 1)
-    sample_std_Sx = std(bitstring_Sx, corrected = true, dims = 1) / sqrt(Nₛ)
+    # # Compute the expectation value and standard deviation of one-point functions based on the samples 
+    # sample_ave_Sx = mean(bitstring_Sx, dims = 1)
+    # sample_std_Sx = std(bitstring_Sx, corrected = true, dims = 1) / sqrt(Nₛ)
 
 
     #************************************************************************************ 
@@ -228,13 +270,12 @@ let
     #************************************************************************************
     Sz_deterministic = Array{Float64}(undef, N)
     Prob = Array{Float64}(undef, N)
-    N_deterministic = 1024                              # Number of deterministic samples
-
+    N_deterministic = 8                                 # Number of deterministic samples
 
     # Sample the first site
     # dsample = []
     ψ_copy = deepcopy(ψ)
-    dsample = single_site_dsample(ψ_copy, 1, "Sx")
+    dsample = single_site_dsample(ψ_copy, 1, "Sx", "")
     Sz_deterministic[1] = 0.5 * (dsample[1][2] - dsample[2][2])
     Prob[1] = dsample[1][2] + dsample[2][2]
     deterministic_bitstring = [dsample[1][1], dsample[2][1]]
@@ -259,20 +300,12 @@ let
             ψ_update = MPS(ψ_tmp[index : N])
             # normalize!(ψ_update)
             
-            tmp_sample = single_site_dsample(ψ_update, 1, "Sx")
+            tmp_sample = single_site_dsample(ψ_update, 1, "Sx", tmp[1])
             # @show length(ψ_update), length(tmp_sample)
             
             push!(dsample, tmp_sample[1])
             push!(dsample, tmp_sample[2])   
 
-            # # @show tmp_string, tmp_string * tmp_sample[1][1], tmp_string * tmp_sample[2][1]
-            # push!(deterministic_bitstring, tmp_string * tmp_sample[1][1])
-            # push!(deterministic_bitstring, tmp_string * tmp_sample[2][1])
-            
-
-            # if length(dsample) != length(deterministic_bitstring)
-            #     error("deterministic sample: the length of projection vectors and bitstrings are not equal.")
-            # end
 
             # @show index, tmp_sample[1][2], tmp_sample[2][2]
             Sz_deterministic[index] += 0.5 * (tmp_sample[1][2] - tmp_sample[2][2])
@@ -295,32 +328,61 @@ let
             sorted_dsample = sort(dsample, by = x -> x[2], rev = true)
             dsample = sorted_dsample[1 : N_deterministic]
             @show length(dsample)
-            @show dsample[1][2], dsample[2][2], dsample[3][2], dsample[4][2], dsample[5][2], dsample[6][2]
+            # @show dsample[1][2], dsample[2][2], dsample[3][2], dsample[4][2], dsample[5][2], dsample[6][2]
         end
     end
+
+    # # Compute the expectation value and standard deviation of two-point functions based on samples generated in deterministic sampling 
+    # @show length(dsample)
+    # if length(dsample) != N_deterministic
+    #     error("The number of deterministic samples is not equal to upper bound set up in the beginning of the code.")
+    # end
     
-    # # @show Sz 
-    # # @show Sz_deterministic
+    dstate = Array{Float64}(undef, length(dsample), N + 1)
+    for (index, tmp_sample) in enumerate(dsample)
+        # Store the orobability of the sample in the first column
+        dstate[index, 1] = tmp_sample[2]
+
+        # Convert the bitstring and store it starting from the second column
+        tmp_array = float( [digit - '0' for digit in tmp_sample[1]])
+        tmp_array[tmp_array .== 1] .= 0.5
+        tmp_array[tmp_array .== 2] .= -0.5
+        dstate[index, 2 : N + 1] = tmp_array
+
+        # @show dstate[index, :]
+    end
+
+    dcorrelation = Array{Float64}(undef, length(dsample), N * N)
+    for index in 1 : length(dsample)
+        for i = 2 : N + 1 
+            for j = 2 : N + 1
+                # @show  dstate[index, 1] * dstate[index, i] * dstate[index, j]
+                dcorrelation[index, (i - 2) * N + j - 1] = (dstate[index, 1] * dstate[index, i] * dstate[index, j]) / Prob[N]
+            end
+        end
+    end
+
+    dcorrelation_ave = mean(dcorrelation, dims = 1)
+    dcorrelation_std = std(dcorrelation, corrected = true, dims = 1) / sqrt(N_deterministic)
+
     # @show Prob
     # # @show state_probability
-    # # @show bitstring
-    # state_distribution = zip(deterministic_bitstring, state_probability)
 
-    # # Save results to a file
-    # h5open("data/Transverse_Ising_N$(N)_h$(h)_Sample$(N_deterministic).h5", "w") do file
-    #     write(file, "Sx", Sx)
-    #     write(file, "Sz", Sz)
-    #     write(file, "Czz", Czz)
-    #     write(file, "Sz ave.", sample_ave_Sz)
-    #     write(file, "Sz err.", sample_std_Sz)
-    #     write(file, "Sx ave.", sample_ave_Sx)
-    #     write(file, "Sx err.", sample_std_Sx)
-    #     write(file, "Czz ave.", sample_ave_Czz)
-    #     write(file, "Czz err.", sample_std_Czz)
-    #     write(file, "Deterministic Sample Sx", Sz_deterministic)
-    #     write(file, "Deterministic Sample Probability", Prob)
-    #     write(file, "State Probability", state_probability)
-    #     # write(file, "State Distribution", state_distribution)
-    # end
-
+    # Save results to a file
+    h5open("data/Transverse_Ising_N$(N)_h$(h)_Sample$(N_deterministic).h5", "w") do file
+        write(file, "Sx", Sx)
+        write(file, "Sz", Sz)
+        write(file, "Czz", Czz)
+        # write(file, "Sz ave.", sample_ave_Sz)
+        # write(file, "Sz err.", sample_std_Sz)
+        # write(file, "Sx ave.", sample_ave_Sx)
+        # write(file, "Sx err.", sample_std_Sx)
+        # write(file, "Czz ave.", sample_ave_Czz)
+        # write(file, "Czz err.", sample_std_Czz)
+        write(file, "Deterministic Sample Sx", Sz_deterministic)
+        write(file, "Deterministic Sample Probability", Prob)
+        write(file, "State Probability", state_probability)
+        write(file, "Deterministic Correlation ave.", dcorrelation_ave)
+        write(file, "Deterministic Correlation err.", dcorrelation_std)
+    end
 end
