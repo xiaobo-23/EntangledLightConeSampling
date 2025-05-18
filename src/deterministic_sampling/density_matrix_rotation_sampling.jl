4f2b1601988b2ed3878efcbbfa4c8f12d64afbd9
Bo Xiao <bxiao@flatironinstitute.org>
2025-05-17 23:27:18 -0400

    Implement the function to compute local observables & need to figure out the rotation matrix

--- src/deterministic_sampling/density_matrix_rotation_sampling.jl
+++ src/deterministic_sampling/density_matrix_rotation_sampling.jl
@@ -98,13 +98,18 @@ end
 
 
 let
-    # Initialize the random MPS
+    # Initialize the system and set up parameters
     N = 8                                    # Number of physical sites
     h = 10.0                                 # Strength of the transverse field
     Nₛ_density = 10000                       # Number of states to be generated in the density matrix rotation sampling 
     # Nₛ = 100                                # Number of bitstrings to be generated according to the Born rule
     # prob_epsilon = 0.0002                  # Probability threshold for truncating the number of samples
    
+    # Initialize a container to store the projected states 
+    projected_states = Vector{Dict{String, Any}}()
+    density_Sz = zeros(Float64, N)
+    Prob = zeros(Float64, N)
+    Prob_density = Vector{Vector{Float64}}()
     
     
     # # Read in the wavefunction from the file and start the sampling process
@@ -124,24 +129,28 @@ let
     # Compute the ground-state wavefunction using DMRG
     sites = siteinds("S=1/2", N; conserve_qns = false) 
     state = [isodd(n) ? "Up" : "Dn" for n = 1 : N]
+    ψ = random_mps(sites, state; linkdims = 3)
 
-    os = OpSum()
-    for j = 1 : N - 1
-        os += "Sz", j, "Sz", j + 1
-        os += h, "Sz", j
-        # os += h, "Sx", j
-    end
-    os += h, "Sz", N
-    # os += h, "Sx", N 
-    H = MPO(os, sites)
-    ψ₀ = randomMPS(sites, state, linkdims = 2)
+    # os = OpSum()
+    # for j = 1 : N - 1
+    #     os += "Sz", j, "Sz", j + 1
+    #     os += h, "Sz", j
+    #     # os += h, "Sx", j
+    # end
+    # os += h, "Sz", N
+    # # os += h, "Sx", N 
+    # H = MPO(os, sites)
+    # ψ₀ = randomMPS(sites, state, linkdims = 2)
 
-    cutoff = [1E-10]
-    nsweeps = 10
-    maxdim = [10,20,100,100,200]
-    energy, ψ = dmrg(H, ψ₀; nsweeps,maxdim,cutoff)
+    # cutoff = [1E-10]
+    # nsweeps = 10
+    # maxdim = [10,20,100,100,200]
+    # energy, ψ = dmrg(H, ψ₀; nsweeps,maxdim,cutoff)
+    
+
+    Sx = expect(ψ, "Sx"; sites = 1 : N)
     Sz = expect(ψ, "Sz"; sites = 1 : N)
-    @show Sz
+    @show Sx, Sz
     
     # #***********************************************************************************
     # # Generate bistrings in the Sx basis according to the Born rule
@@ -198,10 +207,6 @@ let
     # @show U, S, V 
     # @show norm(rho - U * S * V) <= 10 * eps() * norm(rho)
 
-    # Initialize a container to store the projected states
-    projected_states = Vector{Dict{String, Any}}()
-    density_sampling_Sz = Array{Float64}(undef, N)
-
     # Remove prime indices from the first tensor of the MPS
     noprime!(ψ[1])
     tmp_site = siteind(ψ, 1)
@@ -224,20 +229,27 @@ let
             "state" => ψn
         ))
     end
-    projected_states = sort(projected_states, by = x -> x["eigenvalue"], rev = true)
+    # projected_states = sort(projected_states, by = x -> x["eigenvalue"], rev = true)
 
-    # Check the total probability of the projected states
-    println("")
-    println("")
-    @show compute_probability(projected_states, "eigenvalue")
-    println("")
-    println("")
-
-    density_sampling_Sz[1] = sum(
-        state["eigenvalue"] * 0.5 * (state["eigenvector"][1] - state["eigenvector"][2])
-        for state in projected_states
+    Prob[1] = sum(
+        state["eigenvalue"] for state in projected_states
     )
-    @show density_sampling_Sz[1]
+    @show Prob[1]
+    push!(Prob_density, [state["eigenvalue"] for state in projected_states])
+
+    rotation_matrix = [projected_states[1]["eigenvector"][1] projected_states[2]["eigenvector"][1];
+                       projected_states[1]["eigenvector"][2] projected_states[2]["eigenvector"][2]]
+    eigenvalues = [projected_states[1]["eigenvalue"], projected_states[2]["eigenvalue"]]
+    @show rotation_matrix, eigenvalues
+    rotated_eigenvalues = rotation_matrix * eigenvalues
+    @show rotated_eigenvalues[1], rotated_eigenvalues[2]
+    @show 0.5 * (rotated_eigenvalues[1] - rotated_eigenvalues[2])
+    density_Sz[1] = sum(
+        state["eigenvalue"] * (-1)^index * 0.5 * (state["eigenvector"][1] - state["eigenvector"][2])
+        for (index, state) in enumerate(projected_states)
+    )
+    @show density_Sz[1]
+
 
     for idx1 in 2 : N
         # println(" ")
@@ -304,15 +316,24 @@ let
 
         # Sort the projected states based on the eigenvalues and compute local observables based on the projected states
         projected_states = sort(projected_states, by = x -> x["eigenvalue"], rev = true)
-        density_sampling_Sz[idx1] = sum(
+        density_Sz[idx1] = sum(
             state["eigenvalue"] * 0.5 * (state["eigenvector"][1] - state["eigenvector"][2])
             for state in projected_states
         )
+        Prob[idx1] = sum(
+            state["eigenvalue"] for state in projected_states
+        )
+        @show Prob[idx1]
+        push!(Prob_density, [state["eigenvalue"] for state in projected_states])
     end
+
 
     # Check the total probability of the projected states
     @show compute_probability(projected_states, "eigenvalue")
-    @show density_sampling_Sz
+    @show density_Sz
+    @show Sz 
+    @show Prob 
+    # @show Prob_density
 
     # sorted_projected_states = sort(projected_states, by = x -> x["eigenvalue"], rev = true)
     # for i in 1 : 5
@@ -324,223 +345,17 @@ let
     #     println(" ")
     # end
 
-    # #************************************************************************************ 
-    # # Sample the ground-state wavefunction using deterministic sampling
-    # #************************************************************************************
-    # Sz_deterministic = Array{Float64}(undef, N)
-    # Prob = Array{Float64}(undef, N)
-    # N_deterministic = 1024                              # Number of deterministic samples
-
-    # # Sample the first site
-    # # dsample = []
-    # ψ_copy = deepcopy(ψ)
-    # dsample = single_site_dsample(ψ_copy, 1, "Sz", "")
-    # Sz_deterministic[1] = 0.5 * (dsample[1][2] - dsample[2][2])
-    # Prob[1] = dsample[1][2] + dsample[2][2]
-    # deterministic_bitstring = [dsample[1][1], dsample[2][1]]
-    # # @show Sz_deterministic[1], Sz[1], Sx[1]
-    # # @show dsample[1][2] + dsample[2][2]
-    # # @show deterministic_bitstring
-
-    # # Initialize the probability distribution to zeros for better numerical stability
-    # # state_probability = Vector{Float64}()
-    # # probability_density = Array{Float64}(undef, N, N_deterministic)
-    # state_probability = zeros(Float64, 2 * N_deterministic)
-    # probability_density = zeros(Float64, N, N_deterministic)
-
+    #*********************************************************************************************************
+    # Save results into a HDF5 file
+    #*********************************************************************************************************
     
-    # for index in 2 : N
-    #     iteration = length(dsample)
-    #     for _ in 1 : iteration
-    #         # @show typeof(dsample)
-    #         # @show index, dsample[index][1], dsample[index][2]
-    #         tmp = popfirst!(dsample)
-    #         # tmp_string = popfirst!(deterministic_bitstring)
-
-    #         ψ_tmp = deepcopy(ψ_copy)
-    #         # @show typeof(ψ_tmp[index]), ψ_tmp[index]
-    #         ψ_tmp[index] = tmp[3] * ψ_tmp[index]    
-    #         # # normalize!(ψ_tmp)
-    #         # @show typeof(ψ_tmp[index]), ψ_tmp[index]
-    #         ψ_update = MPS(ψ_tmp[index : N])
-    #         # normalize!(ψ_update)
-            
-    #         tmp_sample = single_site_dsample(ψ_update, 1, "Sz", tmp[1])
-    #         # @show length(ψ_update), length(tmp_sample)
-            
-    #         push!(dsample, tmp_sample[1])
-    #         push!(dsample, tmp_sample[2])   
-
-    #         # @show index, tmp_sample[1][2], tmp_sample[2][2]
-    #         Sz_deterministic[index] += 0.5 * (tmp_sample[1][2] - tmp_sample[2][2])
-    #         Prob[index] += tmp_sample[1][2] + tmp_sample[2][2]
-    #         if index == N 
-    #             push!(state_probability, tmp_sample[1][2])
-    #             push!(state_probability, tmp_sample[2][2])
-    #         end
-    #     end
-    #     Sz_deterministic[index] = Sz_deterministic[index] / Prob[index]
-
-    #     # println(" ")
-    #     # println(" ")
-    #     # @show Sz_deterministic[index], Prob[index]
-    #     # # @show length(dsample)
-    #     # # @show Prob[index]
-    #     # println(" ")
-    #     # println(" ")
-
-    #     # Truncated the number of deterministic samples to keep if the number of samples is larger than some threshold
-    #     if length(dsample) > N_deterministic
-    #         sorted_dsample = sort(dsample, by = x -> x[2], rev = true)
-    #         dsample = sorted_dsample[1 : N_deterministic]
-    #         # @show length(dsample)
-    #         # @show dsample[1][2], dsample[2][2], dsample[3][2], dsample[4][2], dsample[5][2], dsample[6][2]
-    #     end
-
-    #     # @show dsample[1][2], dsample[2][2], dsample[3][2]
-    #     tmp_prob_density = Array{Float64}(undef, length(dsample))
-    #     for index in 1 : length(dsample)
-    #         # @show dsample[index][2] 
-    #         tmp_prob_density[index] = dsample[index][2]
-    #     end
-    #     probability_density[index, 1 : length(tmp_prob_density)] = tmp_prob_density
-    #     # @show probability_density[index, 1 : end]
-
-
-    #     # # Truncated the number of deterministic samples based on the upper bound or the probability threshold
-    #     # if length(dsample) > N_deterministic
-    #     #     sorted_dsample = sort(dsample, by = x -> x[2], rev = false)
-    #     #     @show length(sorted_dsample)
-    #     #     # @show sorted_dsample[1][2], sorted_dsample[2][2], sorted_dsample[3][2]
-            
-    #     #     # Compute the cumulative probability of the sorted samples from the smallest to the largest
-    #     #     index_label = 0
-    #     #     tmp_prob_sum = 0.0
-    #     #     for index in 1 : length(dsample)
-    #     #         tmp_prob_sum += sorted_dsample[index][2]
-    #     #         @show tmp_prob_sum
-    #     #         if tmp_prob_sum >= prob_epsilon
-    #     #             index_label = index
-    #     #             break
-    #     #         end
-    #     #     end
-    #     #     cutoff_index = length(sorted_dsample) - index_label + 1
-    #     #     @show index_label, cutoff_index
-    #     #     # @show length(sorted_dsample[index_label : end])
-            
-
-    #     #     sorted_dsample_update = sort(dsample, by = x -> x[2], rev = true) 
-    #     #     if cutoff_index >= N_deterministic
-    #     #         dsample = sorted_dsample_update[1 : N_deterministic]
-    #     #     else
-    #     #         dsample = sorted_dsample_update[1 : cutoff_index]
-    #     #     end
-    #     #     @show length(dsample)
-
-    #     #     # @show index_label, length(dsample) - N_deterministic + 1 
-    #     #     # if length(dsample) - index_label + 1 > N_deterministic
-    #     #     #     dsample = sorted_dsample[length(dsample) - N_deterministic + 1 : end]
-    #     #     #     println("True.")
-    #     #     # else
-    #     #     #     dsample = sorted_dsample[index_label : length(dsample)]
-    #     #     #     @show length(dsample)
-    #     #     #     # @show sorted_dsample[index_label : length(dsample)]
-    #     #     #     println("Flase.")
-    #     #     # end    
-    #     #     # @show dsample[1][2], dsample[2][2], dsample[3][2], dsample[4][2], dsample[5][2], dsample[6][2]
-    #     # end
+    # h5open("data/DMRS_SDKI_N$(N)_h$(h)_Sample$(N_deterministic).h5", "w") do file
+    #     write(file, "Sx", Sx)
+    #     write(file, "Sz", Sz)
+    #     write(file, "Density Sample Sz", density_Sz)
+    #     write(file, "Probability", Prob)
+    #     write(file, "Probability Density", Prob_density)
     # end
-    # @show Sz_deterministic
-    # # @show typeof(probability_density)
-    # # @show size(probability_density)
-    # # @show probability_density[2, 1 : end]
 
-    # # # # Compute the expectation value and standard deviation of two-point functions based on samples generated in deterministic sampling 
-    # # # # @show length(dsample)
-    # # # # if length(dsample) != N_deterministic
-    # # # #     error("The number of deterministic samples is not equal to upper bound set up in the beginning of the code.")
-    # # # # end
-    
-
-    # # sample_length = length(dsample)
-    # # dstate = Array{Float64}(undef, sample_length, N + 1)
-    # # sample_copy = deepcopy(dsample)
-    # # for index in 1 : sample_length
-    # #     # Store the orobability of the sample in the first column
-    # #     dstate[index, 1] = sample_copy[index][2]
-        
-    # #     # Convert the bitstring and store it starting from the second column
-    # #     tmp_array = float([digit - '0' for digit in sample_copy[index][1]])
-    # #     tmp_array[tmp_array .== 1] .= 0.5
-    # #     tmp_array[tmp_array .== 2] .= -0.5
-    # #     dstate[index, 2 : N + 1] = tmp_array
-    # # end
-    
-    # # for (index, nsample) in enumerate(sample_copy)
-    # #     # Store the orobability of the sample in the first column
-    # #     dstate[index, 1] = nsample[2]
-
-    # #     # Convert the bitstring and store it starting from the second column
-    # #     tmp_array = float([digit - '0' for digit in nsample[1]])
-    # #     tmp_array[tmp_array .== 1] .= 0.5
-    # #     tmp_array[tmp_array .== 2] .= -0.5
-    # #     dstate[index, 2 : N + 1] = tmp_array
-    # # end
-    # # dstate_copy = deepcopy(dstate)
-    # # for index in 1 : length(dsample)
-    # #     dstate_copy[index, 2 : N + 1] *= dstate_copy[index, 1]
-    # # end
-    # # dSz_ave = sum(dstate_copy[:, 2 : N + 1], dims = 1) / Prob[N]
-    # # @show dSz_ave
-    # # @show Sz_deterministic
-
-
-    # # # final_prob = sum(dstate[:, 1])
-    # # # println(" ")
-    # # # println(" ")
-    # # # println("The sum of the probabilities of the deterministic samples is $(final_prob).")
-    # # # # if abs(1.0 - final_prob) > 1E-8
-    # # # #     error("The sum of the probabilities of the deterministic samples is not equal to 1.")
-    # # # # end
-    # # # println(" ")
-    # # # println(" ")
-
-    # # dcorrelation = Array{Float64}(undef, length(dsample), N * N)
-    # # dstate_copy = deepcopy(dstate)
-    # # for index in 1 : length(dsample)
-    # #     for i = 2 : N + 1 
-    # #         for j = 2 : N + 1
-    # #             # @show  dstate[index, i] * dstate[index, j]
-    # #             dcorrelation[index, (i - 2) * N + j - 1] = dstate_copy[index, i] * dstate_copy[index, j]
-    # #         end
-    # #     end
-    # #     dcorrelation[index, 2 : N + 1] *= dstate_copy[index, 1]
-    # # end
-
-    # # dcorrelation_ave = sum(dcorrelation, dims = 1) / Prob[N]
-    # # @show dcorrelation_ave[11 : 20] / 1024
-
-    # # @show Prob
-    # # # @show state_probability
-
-    # # # Save results to a file
-    # # h5open("data/DMRS_SDKI_N$(N)_h$(h)_Sample$(N_deterministic).h5", "w") do file
-    # #     write(file, "Sx", Sx)
-    # #     write(file, "Sz", Sz)
-    # #     write(file, "Czz", Czz)
-    # #     # write(file, "Sz ave.", sample_ave_Sz)
-    # #     # write(file, "Sz err.", sample_std_Sz)
-    # #     # write(file, "Sx ave.", sample_ave_Sx)
-    # #     # write(file, "Sx err.", sample_std_Sx)
-    # #     # write(file, "Czz ave.", sample_ave_Czz)
-    # #     # write(file, "Czz err.", sample_std_Czz)
-    # #     write(file, "Deterministic Sample Sx", Sz_deterministic)
-    # #     write(file, "Deterministic Sample Probability", Prob)
-    # #     write(file, "State Probability", state_probability)
-    # #     write(file, "Probability Per Step", probability_density)
-    # #     # write(file, "Deterministic Sx ave.", dSz_ave)
-    # #     # write(file, "Deterministic Correlation ave.", dcorrelation_ave)
-    # # end
     return
-    
 end
